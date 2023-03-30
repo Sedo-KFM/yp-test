a00799cf3571f379879735d01cdc1be7fd6332da
Sedo-KFM <fedor122448@gmail.com>
2023-03-30 13:50:14 +0400

    с1

--- src/main/java/com/kiriakov/magtrain/Database.java
+++ src/main/java/com/kiriakov/magtrain/Database.java
@@ -8,12 +8,10 @@ import java.util.HashMap;
 
 public class Database {
     static HashMap<Integer, Group> groups;
-//    static HashMap<Integer, Participant> participants;
     private static int nextId = 1;
 
     static {
         groups = new HashMap<>();
-//        participants = new HashMap<>();
     }
 
     static int takeNextId() {
